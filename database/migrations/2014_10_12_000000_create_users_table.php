d5a47d96dcdb2ccc3fcbf22c0ec9fccfab1d70a4
fuug <m.skalaban@gmail.com>
2022-01-13 20:37:10 +0300

    Change default val for user role

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role')->default('user');
+            $table->string('role')->default('customer');
             $table->string('thumbnail');
             $table->rememberToken();
             $table->timestamps();
