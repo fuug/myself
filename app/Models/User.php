9aa03ee675cac5cbf4f4104023eb60afdf6c27ca
fuug <m.skalaban@gmail.com>
2022-01-21 15:42:15 +0300

    user and calendar styles. add verified email and gender to users

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
@@ -25,6 +25,7 @@ class User extends Authenticatable
         'role',
         'role_rus',
         'description',
+        'gender',
         'thumbnail',
     ];
 
