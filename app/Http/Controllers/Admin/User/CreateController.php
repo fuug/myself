e43a62c3e0659dc59f89e49f2f0c5e31df3ad6b5
fuug <m.skalaban@gmail.com>
2022-01-21 15:39:29 +0300

    send mail

--- app/Http/Controllers/Admin/User/CreateController.php
+++ app/Http/Controllers/Admin/User/CreateController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\Admin\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\User\CreateRequest;
+use App\Mail\User\PasswordMail;
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
 
 class CreateController extends Controller
@@ -13,14 +17,17 @@ class CreateController extends Controller
     public function __invoke(CreateRequest $request)
     {
         $data = $request->validated();
-        User::create([
+        $password = Str::random(10);
+        $user = User::create([
             'name' => $data['username'],
             'email' => $data['email'],
             'role' => $data['user_role'],
+            'thumbnail' => 'images/guest.png',
             'role_rus' => $this->getRoleName($data['user_role']),
-            'password' => Hash::make('11111111'),
+            'password' => Hash::make($password),
         ]);
-
+        Mail::to($data['email'])->send(new PasswordMail($password));
+        event(new Registered($user));
         return redirect()->route('admin.user.index');
     }
 
