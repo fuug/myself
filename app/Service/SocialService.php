1888696aa4e4f64503f459c36aee60f3290c2a3c
fuug <m.skalaban@gmail.com>
2022-01-26 09:39:50 +0300

    password generation and send mail in SocialService

--- app/Service/SocialService.php
+++ app/Service/SocialService.php
@@ -2,16 +2,19 @@
 
 namespace App\Service;
 
+use App\Mail\User\PasswordMail;
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
 
 class SocialService
 {
     public function saveSocialData($user)
     {
-        $password = Hash::make('11111111');
+        $password = Str::random(10);
 
         $isUser = User::where('email', $user->email)->first();
         if ($isUser) {
@@ -24,7 +27,8 @@ class SocialService
             'email' => $user->email,
             'password' => $password
         ]);
-
+        Mail::to($user->email)->send(new PasswordMail($password));
+        event(new Registered($user));
         Auth::login($createUser);
 
 
