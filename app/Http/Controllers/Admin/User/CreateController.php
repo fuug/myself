c1af159f6d07f848c9fcaf6e2b3b0b64312a336e
fuug <m.skalaban@gmail.com>
2022-01-31 16:37:26 +0300

    Create performer edit

--- app/Http/Controllers/Admin/User/CreateController.php
+++ app/Http/Controllers/Admin/User/CreateController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin\User;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\User\CreateRequest;
 use App\Mail\User\PasswordMail;
+use App\Models\PerformerDescription;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Hash;
@@ -27,6 +28,13 @@ class CreateController extends Controller
         ]);
         Mail::to($data['email'])->send(new PasswordMail($password));
         event(new Registered($user));
+
+        if ($user->role->title == 'performer') {
+            PerformerDescription::create([
+                "user_id"=> $user->id
+            ]);
+        }
+
         return redirect()->route('admin.user.index');
     }
 }
